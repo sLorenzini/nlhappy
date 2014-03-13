a87fb8e0b8ce88dfd0141afb46bb3b691089ddde
fram <fm.de.jouvencel@gmail.com>
2014-03-13 15:02:27 +0100

    // added sample config file

---
+++ config/db.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'nlhappy',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_general_ci'
+);
